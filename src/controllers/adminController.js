30b3738b49415570dd0a298377771d17cf996733
Edward Nguyen <ed@delimister.com>
2024-12-02 00:38:06 +0700

    :sparkles: total dashboard overview

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -5,11 +5,18 @@ const Comment = require('../models/Comment');
 const Category = require('../models/Category');
 
 const getDashboard = async (req, res) => {
+  const totalUsers = await User.count({}).exec();
+  const totalProjects = await Project.count({}).exec();
+  const totalComments = await Comment.count({}).exec();
   const latestProjects = await Project.find({}).populate('author', 'fullName email').sort('-createdAt').limit(10).exec();
   const latestComments = await Comment.find({}).populate('author', 'fullName email').sort('-createdAt').limit(5).exec();
   const mostVotedProjects = await Project.find({}).populate('author', 'fullName email').sort({likes: -1}).limit(10).exec();
 
   res.json({
+    totalUsers,
+    totalProjects,
+    totalComments,
+
     latestProjects,
     latestComments,
     mostVotedProjects,
